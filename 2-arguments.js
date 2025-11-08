7c859cbb052e3b37747ca99c861d54191ea3b0bc
Brolyne <shamalabrolyne@gmail.com>
2025-11-08 16:57:28 +0300

    more fixes

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,10 @@
 const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 if(!arg1){
     console.log("No argument");
+}
+if(!arg2){
+     console.log("Argument found");
 }
 else{
     console.log("Arguments found");
