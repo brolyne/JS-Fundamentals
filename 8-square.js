e476ff9fb865589e334becdf4b79fb8362b18617
Brolyne <shamalabrolyne@gmail.com>
2025-11-08 16:52:03 +0300

    more fixes

--- 8-square.js
+++ 8-square.js
@@ -3,8 +3,8 @@ if(!process.argv[2]){
     console.log("Missing size");
     return;
 }
-for(let i=0;i<count;i++){
-    for(let j=0;j<count;j++){
+for (let i=0;i<count;i++){
+    for (let j=0;j<count;j++){
         process.stdout.write("X");
     }
     console.log();
